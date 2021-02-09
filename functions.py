23b592e588912ca593e46106542451fa9264c048
Demesmaeker <morgane.demesmaeker@gmail.com>
2021-02-09 13:11:24 +0100

    adding surface to the distance dataset

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import pandas as pd
+surfaces_df = pd.read_excel('data/absSurfs.xlsx', engine='openpyxl')
+
 def qtt_pesticides(row, *args):
     total = 0
     for arg in args:
@@ -12,3 +15,10 @@ def nb_pesticides(row, *args):
         if row[arg] > 0:
             total += 1
     return total
+
+
+def get_surface(row):
+    site = row['Site']
+    classCLC = str(row['classCLC'])
+    surface = surfaces_df[surfaces_df['Site'] == site][classCLC].values[0]
+    return surface
