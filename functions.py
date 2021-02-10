ebd07806465c3a1c8d38479bfcf9b5ef9e518253
Demesmaeker <morgane.demesmaeker@gmail.com>
2021-02-10 10:28:00 +0100

    mlr + groupby for pesticides

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+classe_pesticides_df = pd.read_excel('data/pesticides.xlsx', engine='openpyxl')
 surfaces_df = pd.read_excel('data/absSurfs.xlsx', engine='openpyxl')
 
 def qtt_pesticides(row, *args):
@@ -15,6 +16,12 @@ def nb_pesticides(row, *args):
         if row[arg] > 0:
             total += 1
     return total
+
+def lmr_pesticides(row, *args):
+    for arg in args:
+        if row[arg] >= classe_pesticides_df[classe_pesticides_df['importName'] == arg]['LMR'].values[0]:
+            return 1
+    return 0
 
 def get_surface(row):
     site = row['Site']
