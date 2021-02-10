c4722d5397266c13e4861022924f060df02c87ad
Demesmaeker <morgane.demesmaeker@gmail.com>
2021-02-10 12:59:10 +0100

    sites with area/distance for every zone

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 classe_pesticides_df = pd.read_excel('data/pesticides.xlsx', engine='openpyxl')
 surfaces_df = pd.read_excel('data/absSurfs.xlsx', engine='openpyxl')
+distances_df = pd.read_excel('data/distsOneSheet.xlsx', engine='openpyxl')
 
 def qtt_pesticides(row, *args):
     total = 0
@@ -28,3 +29,16 @@ def get_surface(row):
     classCLC = str(row['classCLC'])
     surface = surfaces_df[surfaces_df['Site'] == site][classCLC].values[0]
     return surface
+
+def distance_area(row, CLC):
+    site = row['Site']
+    area = row[CLC]
+    distance = distances_df[(distances_df['Site'] == site) & 
+                            (distances_df['classCLC'] == int(CLC))]['dist'].values
+    if len(distance) > 1 :
+        distance = distance.mean()
+    elif (len(distance) == 0) or (distance[0] < 1) :
+        distance = 1
+    else :
+        distance = distance[0]
+    return area/distance
